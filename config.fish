a914b7cf1d3796336e8f7b3110ba2f66c0614ae0
Gustavo Warmling Teixeira <gustavowt@gmail.com>
2013-10-29 14:58:55 -0200

    Add rbenv configs

--- config.fish
+++ config.fish
@@ -1,4 +1,11 @@
-set -x PATH $PATH /sbin/
+set -x PATH /usr/local/bin $PATH
+set -x PATH /usr/local/heroku/bin $PATH
+
 set -U EDITOR mvim
 set RUBY_GC_MALLOC_LIMIT 60000000
 set RUBY_FREE_MIN 200000
+
+#rbenv
+set -x PATH $HOME/.rbenv/bin $PATH
+set -x PATH $HOME/.rbenv/shims $PATH
+rbenv rehash >/dev/null ^&1
