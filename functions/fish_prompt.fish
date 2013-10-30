0806ce2b8dedba98b7d2f267bd40f0d2af1a6cc3
Gustavo Warmling Teixeira <gustavowt@gmail.com>
2013-10-30 11:40:29 -0200

    fish_prompt improvement on branch funciont

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -1,21 +1,11 @@
 set fish_git_dirty_color red
-set fish_git_not_dirty_color red
 
 function parse_git_branch
-  set -l branch (git branch 2> /dev/null | grep -e '\* ' | sed 's/^..\(.*\)/\1/')
-  set -l git_diff (git diff)
+  set -l branch (git branch 2>/dev/null | grep -e '^*' | sed -E 's/^\* (.+)$/(\1) /')
 
-  if test -n "$git_diff"
-    echo (set_color $fish_git_dirty_color)$branch(set_color red)
-  else
-    echo (set_color $fish_git_not_dirty_color)$branch(set_color red)
-  end
+  echo (set_color $fish_git_dirty_color)$branch(set_color red)
 end
 
 function fish_prompt
-  if test -d .git
-    printf '%s%s %s[%s]%s (%s) %s$ ' (set_color green) (whoami) (set_color blue) (prompt_pwd) (set_color red) (parse_git_branch) (set_color normal)
-  else
-    printf '%s%s %s%s%s$ ' (set_color green) (whoami) (set_color blue) (prompt_pwd) (set_color normal)
-  end
+    printf '%s%s %s[%s]%s %s%s$ ' (set_color green) (whoami) (set_color blue) (prompt_pwd) (set_color red) (parse_git_branch) (set_color normal)
 end
