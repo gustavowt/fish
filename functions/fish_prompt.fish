01b4c3414bf9c502ef95b933653a42b1847f639a
Gustavo Warmling Teixeira <gustavowt@gmail.com>
2013-11-19 17:29:55 -0200

    Add ruby version at fish_prompt

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -6,6 +6,10 @@ function parse_git_branch
   echo (set_color $fish_git_dirty_color)$branch(set_color red)
 end
 
+function get_ruby_version
+  echo (ruby --version | cut -d' ' -f2)
+end
+
 function fish_prompt
-    printf '%s%s %s[%s]%s %s%s$ ' (set_color green) (whoami) (set_color blue) (prompt_pwd) (set_color red) (parse_git_branch) (set_color normal)
+    printf '%s%s %s[%s]%s %s%s %s%s$ ' (set_color green) (whoami) (set_color blue) (prompt_pwd) (set_color purple) (get_ruby_version) (set_color red) (parse_git_branch) (set_color normal)
 end
